5b7368df5f668509d146e11e5a43d3cb819287c9
HugoBoxFr <hugo.prea@sfr.fr>
2019-09-28 18:14:44 +0200

    temp

--- scripts.js
+++ scripts.js
@@ -2,15 +2,7 @@ const easyMode = 4;
 const mediumMode = 6;
 const hardMode = 8;
 const double = 2;
-/*
-const cards = document.getElementsByClassName('memory-card');
-let firstCard;
 
-function flipCard() {
-  this.classList.toggle('flip');
-}
-cards.forEach(card => card.addEventListener('click', flipCard));
-*/
 
 const back = 'images/backface.jpg';
 
@@ -30,22 +22,24 @@ let imagesElt = document.getElementById('memory-game');
 
 for(let i = 0; i < images.length; i++) {
   for(let j = 0; j < 2; j++) {
-    imagesElt.innerHTML += `<img src="${back}" style="display: block" id="back-${[i + 1]}"/>`;
-    imagesElt.innerHTML += `<img src="${images[i]}" style="display: none" id="card-${[i + 1]}""/>`;
+    imagesElt.innerHTML += `<img class="front" onclick=“onCardClicked(’${[i]}’)”  style="display: none" src="${back}"  id="back-${[i]}"/>`;
+    imagesElt.innerHTML += `<img class="back" onclick=“onCardClicked(’${[i]}’)”  style="display: block" src="${images[i]}" id="front-${[i]}"/>`;
   };
-  backIndex[i] = document.getElementById(`back-${[i + 1]}`);
-  cartesIndex[i] = document.getElementById(`card-${[i + 1]}`);
+  backIndex[i] = document.getElementById(`back-${[i]}`);
+  cartesIndex[i] = document.getElementById(`card-${[i]}`);
 };
 
 console.log(cartesIndex);
 console.log(backIndex);
-
-
 
 function displayCard() {
   location.reload();
 };
 
 
-
-
+function onCardClicked ([i]) {
+  let backElt = document.getElementById (`back-${[i]}`);
+  let frontElt = document.getElementById (`front-${[i]}`);
+  console.log(backElt)
+  console.log(frontElt);
+};
