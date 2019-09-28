e0c60051c28f557c14063b2f0844b0e3c09b088c
Isabelle Kasas <isabelle.kasas@hotmail.fr>
2019-09-28 13:37:53 +0200

    récupération code hugo

--- scripts.js
+++ scripts.js
@@ -1,46 +1,42 @@
 const easyMode = 4;
-const easyMedium = 6;
-const easyHard = 8;
+const mediumMode = 6;
+const hardMode = 8;
 const double = 2;
-const triple = 3;
-
-const easy = { 
-  cards: [
-      'images/Kenny.png',
-      'images/Cartman.png',
-      'images/Kyle.png',
-      'images/Stan.png',
-      'images/Timmy.png',
-      'images/Wendy.png',
-  ]
-};
-
-const cardsElt = document.getElementById('memory-game');
-
-/* // boucle de base
-for (let i = 0; i < easyMode; i++) {
-  for (let j = 0; j < double; j++) {
-    cardsElt.innerHTML += `<img src="${easy.cards[i]}" />`;
-  }
-};
-
-function displayCard() {
-  alert('le bouton marche !!!')
-};
-*/
-
-function displayCard() {
-  for (let i = 0; i < easyMode; i++) {
-    for (let j = 0; j < double; j++) {
-      cardsElt.innerHTML += `<div class="memory-card" data-framework=""><img class="back-face" src="https://via.placeholder.com/150x150" alt="JS Badge" /><img class="front-face imgCard" src="${easy.cards[i]}" /></div>`;
-    }
-  };
-};
-
+/*
 const cards = document.getElementsByClassName('memory-card');
 let firstCard;
-
 function flipCard() {
   this.classList.toggle('flip');
 }
 cards.forEach(card => card.addEventListener('click', flipCard));
+*/
+
+const back = 'images/backface.jpg';
+
+const images = [
+  'images/Kenny.png',
+  'images/Cartman.png',
+  'images/Kyle.png',
+  'images/Stan.png',
+  'images/Timmy.png',
+  'images/Wendy.png',
+]
+
+const cartesIndex = [];
+const backIndex = [];
+
+let imagesElt = document.getElementById('memory-game');
+
+for (let i = 0; i < images.length; i++) {
+  for (let j = 0; j < 2; j++) {
+    imagesElt.innerHTML += `<img src="${back}" style="display: block" id="back-${[i + 1]}"/>`;
+    imagesElt.innerHTML += `<img src="${images[i]}" style="display: none" id="card-${[i + 1]}""/>`;
+  };
+  backIndex[i] = document.getElementById(`back-${[i + 1]}`);
+  cartesIndex[i] = document.getElementById(`card-${[i + 1]}`);
+};
+console.log(cartesIndex);
+console.log(backIndex);
+function displayCard() {
+  location.reload();
+};
